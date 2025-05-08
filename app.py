476af2e43dc6cfa4b4b1c0b9dd1fe16ca1e802bc
Diego Parra <diego.parra@fashionspark.com>
2025-05-08 16:28:27 -0400

    Primer commit con Docker y workflow

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def home(): return 'Aplicacion CI/CD con Docker Actions Ahora si que si'
+def home(): return 'Aplicacion CI/CD con Docker Actions Ahora si que si ccon Bernardo'
 if __name__ == '__main__': app.run(host='0.0.0.0', port=5000)
